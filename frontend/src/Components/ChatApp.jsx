e9da8bcebee343ecd636a2aa70616c07235bf979
Vinayan AC <vinayanac7777@gmail.com>
2025-07-28 17:22:41 +0530

    updated

--- frontend/src/Components/ChatApp.jsx
+++ frontend/src/Components/ChatApp.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
-const socket = io("http://localhost:1009");
+const socket = io("https://chat-app-jhj9.onrender.com");
 const ChatApp = () => {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
